31667c969c21585f73edc2a9f97a1a915ead98a8
Peter Janacik <peter.janacik@tu-berlin.de>
2015-01-07 12:42:09 +0100

    Back to FloatWritable for Edge Input Format

--- giraph-app/GiraphExper/src/socialnetwork/SocialGraph.java
+++ giraph-app/GiraphExper/src/socialnetwork/SocialGraph.java
@@ -6,16 +6,17 @@ import org.apache.giraph.graph.BasicComputation;
 import org.apache.giraph.graph.Vertex;
 import org.apache.giraph.edge.Edge;
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.log4j.Logger;
 
-public class SocialGraph extends BasicComputation<LongWritable, HashWritable, HashWritable, DoubleWritable> {
+public class SocialGraph extends BasicComputation<LongWritable, HashWritable, FloatWritable, DoubleWritable> {
 	protected static Logger log = Logger.getLogger(SocialGraph.class);
 	
 	
 	@Override
 	public void compute(
-			Vertex<LongWritable, HashWritable, HashWritable> vertex,
+			Vertex<LongWritable, HashWritable, FloatWritable> vertex,
 			Iterable<DoubleWritable> messages) throws IOException {
 
 		// Basic log at a vertex
@@ -25,7 +26,7 @@ public class SocialGraph extends BasicComputation<LongWritable, HashWritable, Ha
 		// Send message with random number along each outgoing
 		// edge.
 		if (getSuperstep() == 0) {
-			for (Edge<LongWritable, HashWritable> edge : vertex.getEdges()) {
+			for (Edge<LongWritable, FloatWritable> edge : vertex.getEdges()) {
 				double randomNumber = Math.random();
 				sendMessage(edge.getTargetVertexId(), 
 					new DoubleWritable(randomNumber));
