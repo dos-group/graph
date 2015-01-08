8d79460621e41f6a766159057950fa17e14fa065
Peter Janacik <peter.janacik@tu-berlin.de>
2015-01-08 11:52:44 +0100

    Added IOEx handling

--- giraph-app/GiraphExper/src/socialnetwork/StringWritable.java
+++ giraph-app/GiraphExper/src/socialnetwork/StringWritable.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.io.WritableComparable;
+import org.apache.log4j.Logger;
 
 /**
  * Writable for Double values.
@@ -14,67 +15,78 @@ import org.apache.hadoop.io.WritableComparable;
 @InterfaceAudience.Public
 @InterfaceStability.Stable
 public class StringWritable implements WritableComparable<StringWritable> {
+	protected static Logger log = Logger.getLogger(SocialGraph.class);
 
-  private String value = "";
-  
-  public StringWritable() {
-    
-  }
-  
-  public StringWritable(String value) {
-    set(value);
-  }
-  
-  @Override
-  public void readFields(DataInput in) throws IOException {
-	  
-	String s = "";
-	char c = in.readChar();
-	while (c != '}') {
-		if (c != '{') {
-			s += c;
-		}
-		c = in.readChar();
+	private String value = "";
+
+	public StringWritable() {
+
 	}
-    value = s;
-    
-  }
 
-  @Override
-  public void write(DataOutput out) throws IOException {
-    out.write(value.getBytes());
-  }
-  
-  public void set(String value) { this.value = value; }
-  
-  public String get() { return value; }
+	public StringWritable(String value) {
+		set(value);
+	}
 
-  /**
-   * Returns true iff <code>o</code> is a DoubleWritable with the same value.
-   */
-  @Override
-  public boolean equals(Object o) {
-    if (!(o instanceof StringWritable)) {
-      return false;
-    }
-    StringWritable other = (StringWritable)o;
-    return this.value == other.value;
-  }
-  
-  @Override
-  public int hashCode() {
-    return value.hashCode();
-  }
-  
-  @Override
-  public int compareTo(StringWritable o) {
-    return value.compareTo(o.value);
-  }
+	@Override
+	public void readFields(DataInput in) throws IOException {
 
-  @Override
-  public String toString() {
-    return value;
-  }
+		String s = "";
+		try {
+
+			char c = in.readChar();
+			while (c != '}') {
+				if (c != '{') {
+					s += c;
+				}
+				c = in.readChar();
+			}
+
+		} catch (Exception e) {
+			log.info("Starting computations at a vertex.");
+			throw e;
+		}
+		value = s;
+
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		out.write(value.getBytes());
+	}
+
+	public void set(String value) {
+		this.value = value;
+	}
+
+	public String get() {
+		return value;
+	}
+
+	/**
+	 * Returns true iff <code>o</code> is a DoubleWritable with the same value.
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof StringWritable)) {
+			return false;
+		}
+		StringWritable other = (StringWritable) o;
+		return this.value == other.value;
+	}
+
+	@Override
+	public int hashCode() {
+		return value.hashCode();
+	}
+
+	@Override
+	public int compareTo(StringWritable o) {
+		return value.compareTo(o.value);
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
 
 }
-
