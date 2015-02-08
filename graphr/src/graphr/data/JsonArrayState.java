8eb6702a26b7fd8fd9658a4606927876cb8072ff
Dalimir <orfanus@gmail.com>
2015-02-08 23:07:11 +0100

    Bugfix: If an array is empty, then JSON string does not have an ending square bracket.

--- graphr/src/graphr/data/JsonArrayState.java
+++ graphr/src/graphr/data/JsonArrayState.java
@@ -19,10 +19,10 @@ public class JsonArrayState {
 		String s = "[";
 		
 		for (int i = 0; i < a.size(); i ++) {
-			s += a.get(i) + (i < a.size() - 1 ? "," : "]");
+			s += a.get(i) + (i < a.size() - 1 ? "," : "");
 		}
 		
-		return s;
+		return s + "]";
 		
 	}
 	
