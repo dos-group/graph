bdf8372b81035850f54a6653d7d1566fb7f135dc
gkreyssing <gk78@gmx.net>
2015-08-12 14:40:17 +0200

    Corrected the methodes i and l inorder to avoid CCE

--- graphr/src/graphr/data/PrimData.java
+++ graphr/src/graphr/data/PrimData.java
@@ -6,20 +6,20 @@ import java.io.Serializable;
 
 public class PrimData implements JsonReadableWritable {
 	private static final long serialVersionUID = -2410903391512626771L;
-	
+
 	Object o;
-	
-	//-- Constructors
-	
+
+	// -- Constructors
+
 	public PrimData(Object o) {
 		if ((o instanceof Integer) || (o instanceof Long) || (o instanceof Double) || (o instanceof Boolean)
-			|| (o instanceof String) || (o == null) ) {	
+				|| (o instanceof String) || (o == null)) {
 			this.o = o;
 		} else {
-			throw new UnsupportedOperationException("Object type unsupoorted by PrimData. "+o.toString());
+			throw new UnsupportedOperationException("Object type unsupoorted by PrimData. " + o.toString());
 		}
 	}
-	
+
 	public PrimData(String abbrev, String someString) {
 		setFromSomeString(abbrev, someString);
 	}
@@ -27,157 +27,196 @@ public class PrimData implements JsonReadableWritable {
 	public PrimData(int i) {
 		set(i);
 	}
-	
+
 	public PrimData(long l) {
 		set(l);
 	}
-	
+
 	public PrimData(double d) {
 		set(d);
 	}
-	
+
 	public PrimData(boolean b) {
 		set(b);
 	}
-	
+
 	public PrimData(String s) {
 		set(s);
 	}
-	
+
 	// -- Setters
 
 	public void set(long l) {
 		o = new Long(l);
 	}
-	
+
 	public void set(int i) {
 		o = new Integer(i);
 	}
-	
+
 	public void set(double d) {
 		o = new Double(d);
 	}
-	
+
 	public void set(boolean b) {
 		o = new Boolean(b);
 	}
-	
+
 	public void set(String s) {
 		o = s;
 	}
-	
+
 	public void set(Object o) {
 		this.o = o;
 	}
-	
+
 	public void setFromSomeString(String abbrev, String s) {
-		switch (abbrev){
-		case "i": set(new Integer(s)); break;
-		case "l": set(new Long(s)); break;
-		case "d": set(new Double(s)); break;
-		case "b": set(new Boolean(s)); break;
-		default: set(s);
+		switch (abbrev) {
+		case "i":
+			set(new Integer(s));
+			break;
+		case "l":
+			set(new Long(s));
+			break;
+		case "d":
+			set(new Double(s));
+			break;
+		case "b":
+			set(new Boolean(s));
+			break;
+		default:
+			set(s);
 		}
-			
+
 	}
-	
-	//-- Getters
-	
+
+	// -- Getters
+
 	public int i() {
-		return ((Integer) o).intValue();
+		if (o instanceof Integer) {
+			return ((Integer) o).intValue();
+		}
+		if (o instanceof Long) {
+			return ((Long) o).intValue();
+		}
+		if (o instanceof Double) {
+			return ((Double) o).intValue();
+		}
+		if (o instanceof String) {
+			return Integer.parseInt((String) o);
+		}
+		if (o instanceof Boolean) {
+			return (((Boolean) o) == true) ? 1 : 0;
+		}
+		throw new IllegalStateException("Wrapped object type is invalid.");
 	}
 
 	public long l() {
-		return ((Long) o).longValue();
+		if (o instanceof Integer) {
+			return ((Integer) o).longValue();
+		}
+		if (o instanceof Long) {
+			return ((Long) o).longValue();
+		}
+		if (o instanceof Double) {
+			return ((Double) o).longValue();
+		}
+		if (o instanceof String) {
+			return Long.parseLong((String) o);
+		}
+		if (o instanceof Boolean) {
+			return (((Boolean) o) == true) ? 1L : 0L;
+		}
+		throw new IllegalStateException("Wrapped object type is invalid.");
 	}
-	
+
 	public double d() {
 		return ((Double) o).doubleValue();
 	}
-	
+
 	public boolean b() {
 		return ((Boolean) o).booleanValue();
 	}
-	
+
 	public String s() {
 		return o.toString();
 	}
-	
+
 	public Object o() {
 		return o;
 	}
 
 	public String toString() {
-		
+
 		if (o instanceof Integer) {
-			return ((Integer) o).toString();  
+			return ((Integer) o).toString();
 		}
 
 		if (o instanceof Long) {
-			return ((Long) o).toString();  
+			return ((Long) o).toString();
 		}
-		
+
 		if (o instanceof Double) {
-			return ((Double) o).toString(); 
+			return ((Double) o).toString();
 		}
-		
+
 		if (o instanceof Boolean) {
-			return ((Boolean) o).toString(); 
+			return ((Boolean) o).toString();
 		}
-		
+
 		if (o instanceof String) {
 			return (String) o;
 		}
-		
+
 		throw new IllegalStateException("Wrapped object type is invalid.");
-		
+
 	}
-	
+
 	public String getDataAbbrev() {
-		
+
 		return PrimData.getDataAbbrev(o);
-		
+
 	}
-	
-	//-- Convenience Methods
-	
+
+	// -- Convenience Methods
+
 	public static String getDataAbbrev(Object o) {
-		
+
 		if (o instanceof Integer) {
-			return "i";  
+			return "i";
 		}
 
 		if (o instanceof Long) {
-			return "l";  
+			return "l";
 		}
-		
+
 		if (o instanceof Double) {
-			return "d"; 
+			return "d";
 		}
-		
+
 		if (o instanceof Boolean) {
-			return "b"; 
+			return "b";
 		}
-		
+
 		if (o instanceof String) {
 			return "S";
 		}
-		
+
 		throw new IllegalArgumentException("Examined object type invalid.");
-		
+
 	}
 
 	@Override
 	public String getAsJson() {
 
 		return o.toString();
-		
+
 	}
 
 	@Override
 	public void setFromJson() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
